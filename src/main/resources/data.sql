6a26f3f56c6fc6ce8e00a8616f6b3825999ecf99
Daniela Paz Bernales <dbernales.miranda@gmail.com>
2026-04-29 16:59:59 -0400

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO ESTUDIANTE
+(nombre, email)
+VALUES
+('Daniela Bernales',
+'daniela@untec.com');
+
+INSERT INTO ESTUDIANTE
+(nombre, email)
+VALUES
+('Carlos Soto',
+'carlos@untec.com');
+
+INSERT INTO CURSO
+(nombre, profesor)
+VALUES
+('Spring Boot',
+'Juan Perez');
+
+INSERT INTO CURSO
+(nombre, profesor)
+VALUES
+('Java Web',
+'Maria Gonzalez');
